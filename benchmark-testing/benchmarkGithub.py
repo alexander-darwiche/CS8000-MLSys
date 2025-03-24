1175f9f701422ea62c2996efe36a3333f32b0317
alexander-darwiche <59787022+alexander-darwiche@users.noreply.github.com>
2025-03-24 15:24:42 -0400

    fixing path

--- benchmark-testing/benchmarkGithub.py
+++ benchmark-testing/benchmarkGithub.py
@@ -57,7 +57,8 @@ for model_name in model_list:
                     fp16=use_fp16
                 )
                 
-                output_path2 = file + "/" + output_path 
+                file_name = os.path.splitext(file)[0]
+                output_path2 = file_name + "/" + output_path 
 
                 # Save transcript
                 output_file = os.path.join(output_path2, f"{os.path.splitext(file)[0]}_{model_name}_fp16_{use_fp16}.txt")
